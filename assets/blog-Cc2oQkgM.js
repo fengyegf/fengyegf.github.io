29860329cddf8d788d27482a625ee7609afa822d
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-03-22 17:37:38 +0000

    deploy: d33075b25ce04518df8282c48ce6d1fdc51bfdd3

---
+++ assets/blog-Cc2oQkgM.js
@@ -0,0 +1,11 @@
+const t={title:"我的博客",published:"2024-12-13T00:00:00.000Z",description:"我第n次更改了我的博客",image:"https://github.com/FY-yun/picx-images-hosting/raw/master/20241217/2293F77B1F19B0A4ED6606A0027F8E46.1ap1yxyt8s.webp",tags:["随手记","快乐分享"],category:"我在人间凑数的日子",draft:!1,lang:""},s=`<h1>我的博客</h1>
+<p>这是我第 n 次换框架和模板了，笑死了。在这一年里，我已经换了 4 次：</p>
+<ol>
+<li><a href="https://hexo.io/">Hexo</a></li>
+<li><a href="https://www.halo.run/">Halo</a></li>
+<li><a href="https://docs.astro.build/zh-cn/getting-started/">Astro</a></li>
+<li><a href="https://wordpress.org/">WordPress</a></li>
+</ol>
+<p>最终还是自己手搓一个博客好看</p>
+<p>接下来就是适配和不断完善了</p>
+`,e=[{level:"1",content:"&#x6211;&#x7684;&#x535A;&#x5BA2;"}];export{t as attributes,s as html,e as toc};
