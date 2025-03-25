8f8c3798b63d2256bd4e9bb812192a682e63bf37
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-03-25 14:21:15 +0000

    deploy: 5deb98c55a72a779e7b2af01a1b9257ad5e9f4e0

---
+++ assets/blog1-C6nYukb8.js
@@ -0,0 +1,8 @@
+const t={title:"博客",published:"2024-12-13T00:00:00.000Z",description:"介绍一下我的博客",image:"https://1drv.ms/i/c/2182f48b953d36f8/UQT4Nj2Vi_SCIIAhjDgAAAAAAAZZmPXSlJM35FY?width=3993&height=2894",tags:["随手记","快乐分享"],category:"我在人间凑数的日子",draft:!1,lang:""},l=`<h1>博客</h1>
+<p>博客本身就是记录开发经验，记录生活的地方，但我想要的是一个简洁而不简单的，这个算是开山作品吧</p>
+<h2>后续</h2>
+<ul>
+<li>使用 astro 进行开发提升性能</li>
+<li>采用卡片样式美化</li>
+</ul>
+`,n=[{level:"1",content:"&#x535A;&#x5BA2;"},{level:"2",content:"&#x540E;&#x7EED;"}];export{t as attributes,l as html,n as toc};
