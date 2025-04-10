d8fd4178b2d7a89fbd6c37d50149b2479bae1c9c
MLeaf-coder <106392719+MLeaf-coder@users.noreply.github.com>
2025-04-10 16:42:39 +0800

    文章重定向

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import gallery from "@/views/gallery.vue";
 import Blogs from "@/views/Blogs.vue";
 import Articles from "@/components/Articles.vue";
 
+const base = import.meta.env.BASE_URL || "/";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
