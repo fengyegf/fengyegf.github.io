dcb4bf604c126648c210f7a9f1097adc85c129f5
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-04-18 17:40:37 +0000

    deploy: 5c939924e72c6929f2d93c1f5c03c0af97f1de14

---
+++ assets/最搞笑的事情莫过于-C_DuvZE9.js
@@ -0,0 +1,4 @@
+const t={title:"最搞笑的事情莫过于",published:"2025-04-17T00:00:00.000Z",image:"assets/img/Resources/banner.jpg",tags:[]},x=`<h2>写了功能自以为有用结果删除了还能跑，有和无一样</h2>
+<p><img src="https://1drv.ms/i/c/2182f48b953d36f8/UQT4Nj2Vi_SCIIAh9DgAAAAAADJAKstJhrMDcCM?width=1202&amp;height=676" alt="图像">
+<img src="https://1drv.ms/i/c/2182f48b953d36f8/UQT4Nj2Vi_SCIIAhxzgAAAAAAN7Ga9T-_0lKzqU?width=1920&amp;height=1080" alt="图像"></p>
+`,s=[{level:"2",content:"&#x5199;&#x4E86;&#x529F;&#x80FD;&#x81EA;&#x4EE5;&#x4E3A;&#x6709;&#x7528;&#x7ED3;&#x679C;&#x5220;&#x9664;&#x4E86;&#x8FD8;&#x80FD;&#x8DD1;&#xFF0C;&#x6709;&#x548C;&#x65E0;&#x4E00;&#x6837;"}];export{t as attributes,x as html,s as toc};
--- assets/最搞笑的事情莫过于-D3YT69m5.js
+++
@@ -1,2 +0,0 @@
-const x={title:"最搞笑的事情莫过于",published:"2025-04-17T00:00:00.000Z",image:"assets/img/Resources/banner.jpg",tags:[]},t=`<h2>写了功能自以为有用结果删除了还能跑，有和无一样</h2>
-`,e=[{level:"2",content:"&#x5199;&#x4E86;&#x529F;&#x80FD;&#x81EA;&#x4EE5;&#x4E3A;&#x6709;&#x7528;&#x7ED3;&#x679C;&#x5220;&#x9664;&#x4E86;&#x8FD8;&#x80FD;&#x8DD1;&#xFF0C;&#x6709;&#x548C;&#x65E0;&#x4E00;&#x6837;"}];export{x as attributes,t as html,e as toc};
