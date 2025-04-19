9efce044146e2891ad62193b02bd6f8dd6f8322a
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-04-19 17:10:25 +0000

    deploy: 964eceb5e6f7c29a9a27530ca70b36b34e23cc1b

---
+++ assets/1-KYj1L5gn.js
@@ -0,0 +1,73 @@
+const n={title:"样式测试",published:"2024-12-23T00:00:00.000Z",description:"内容样式测试",image:"/assets/img/11.jpg",tags:[],category:""},t=`<h1>文本样式</h1>
+<h2>2</h2>
+<h3>3</h3>
+<h1>提醒样式</h1>
+<p>:::info
+这是一条信息提示
+:::</p>
+<p>:::warning
+这是一条警告提示
+:::</p>
+<p>:::success
+操作成功提示
+:::</p>
+<p>:::error
+错误信息提示
+:::</p>
+<p>:::info
+默认信息提示
+:::</p>
+<p>:::info{soft}
+柔和样式的信息提示
+:::</p>
+<p>:::warning{dash}
+虚线样式的警告提示
+:::</p>
+<p>:::success{soft}{noicon}
+没有图标的柔和成功提示
+:::</p>
+<p>:::error{dash}{inline}
+内联样式的虚线错误提示
+:::</p>
+<p>:::info{outline}
+12 unread messages. Tap to see.
+:::</p>
+<p>:::success{outline}
+Your purchase has been confirmed!
+:::</p>
+<p>:::warning{outline}
+Warning: Invalid email address!
+:::</p>
+<p>:::error{outline}
+Error! Task failed successfully.
+:::</p>
+<h1>远程对比图</h1>
+<p>:::Diff
+<img src="https://1drv.ms/i/c/2182f48b953d36f8/UQT4Nj2Vi_SCIIAhXjgAAAAAAFaJAKzxYV5v9Ok?width=5788&amp;height=4092" alt="alt text">
+vs
+<img src="https://1drv.ms/i/c/2182f48b953d36f8/UQT4Nj2Vi_SCIIAhjDgAAAAAAAZZmPXSlJM35FY?width=3993&amp;height=2894" alt="alt text">
+:::</p>
+<h1>本地图像</h1>
+<p><img src="/assets/img/11.jpg" alt="alt text"></p>
+<h1>折叠面板效果</h1>
+<p>:::Accordion{如何创建账号？}
+点击右上角的&quot;注册&quot;按钮，然后按照注册流程操作即可完成账号创建。
+:::</p>
+<p>:::Accordion{如何修改密码？}</p>
+<ol>
+<li>登录到您的账号</li>
+<li>进入账号设置页面</li>
+<li>点击&quot;安全选项&quot;</li>
+<li>选择&quot;更改密码&quot;并按提示操作
+:::</li>
+</ol>
+<p>:::Accordion{支持哪些支付方式？}
+我们支持以下支付方式：</p>
+<ul>
+<li>信用卡支付</li>
+<li>支付宝</li>
+<li>微信支付</li>
+<li>PayPal
+:::</li>
+</ul>
+`,e=[{level:"1",content:"&#x6587;&#x672C;&#x6837;&#x5F0F;"},{level:"2",content:"2"},{level:"3",content:"3"},{level:"1",content:"&#x63D0;&#x9192;&#x6837;&#x5F0F;"},{level:"1",content:"&#x8FDC;&#x7A0B;&#x5BF9;&#x6BD4;&#x56FE;"},{level:"1",content:"&#x672C;&#x5730;&#x56FE;&#x50CF;"},{level:"1",content:"&#x6298;&#x53E0;&#x9762;&#x677F;&#x6548;&#x679C;"}];export{n as attributes,t as html,e as toc};
---
+++ assets/kfc-CZkmNIDG.js
@@ -0,0 +1,7 @@
+const t={title:"kfc联动",published:"2025-04-18T00:00:00.000Z",image:"https://pbs.twimg.com/media/Gn_8DIPaIAAXa-m?format=jpg&name=900x900",tags:["美食"]},s=`<p>:::Accordion{随拍}
+<img src="https://1drv.ms/i/c/2182f48b953d36f8/IQRrrIX-df5_T4_vU_LOrQCsAS2x9usqxE7a-Qm7cp7WSdk?width=4080&amp;height=1836" alt="图1">
+:::
+:::Accordion{徽章}
+<img src="https://1drv.ms/i/c/2182f48b953d36f8/IQTFYzjhbGgJRovAU0ggs4DdAbc25-2fandj32DNN4BgfPo?width=4080&amp;height=1836" alt="图2">
+:::</p>
+`,c=[];export{t as attributes,s as html,c as toc};
