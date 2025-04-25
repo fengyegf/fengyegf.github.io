8418507cd08572e14a63772cbf8dab5c7b606966
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-04-25 16:52:17 +0000

    deploy: 6f72f85b7ec6a20702637fe71fef2392c2a1c0aa

---
+++ assets/扣子-BGyL-nsm.js
@@ -0,0 +1,25 @@
+const i={title:"扣子",published:"2025-04-25T00:00:00.000Z",image:"",tags:[]},e=`<p>最近申请扣子空间也是过了，分享 5 个邀请码</p>
+<p><a href="https://www.coze.cn/space-preview?invite_code=6OSEI6YV">邀请码 1</a>
+<a href="https://www.coze.cn/space-preview?invite_code=E00WI1KK">邀请码 2</a>
+<a href="https://www.coze.cn/space-preview?invite_code=M02PYBD5">邀请码 3</a>
+<a href="https://www.coze.cn/space-preview?invite_code=BF1XC9XW">邀请码 4</a>
+<a href="https://www.coze.cn/space-preview?invite_code=VHBKRX8P">邀请码 5</a></p>
+<p>扣子空间，主要看的是 <strong>Agent</strong> 智能体目前来说只有</p>
+<ul>
+<li>高德地图</li>
+<li>飞书云文档</li>
+<li>飞书多维表格</li>
+<li>飞书电子表格</li>
+<li>水滴信用</li>
+<li>飞常准</li>
+<li>音乐生成</li>
+<li>墨迹天气</li>
+<li>图像工具</li>
+<li>语音合成</li>
+<li>Notion</li>
+<li>Github</li>
+<li>MySQL</li>
+<li>Clickhouse</li>
+</ul>
+<p>不过应该也够了，实用的也就那点</p>
+`,n=[];export{i as attributes,e as html,n as toc};
