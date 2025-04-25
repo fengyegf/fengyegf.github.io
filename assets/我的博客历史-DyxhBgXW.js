cd1596a0f73b08d94aded732b2ba811eda1079e6
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-04-25 16:35:21 +0000

    deploy: 50a0284d8ce838b8e19a0ba21513ded1697bb764

---
+++ assets/我的博客历史-DyxhBgXW.js
@@ -0,0 +1,14 @@
+const t={title:"我的博客历史",published:"2025-04-25T00:00:00.000Z",image:"https://1drv.ms/i/c/2182f48b953d36f8/IQRXsgLi0mDDQJ4SFQAIbshfAbrXUo0QE3un7qO9NWMCtAs?width=3171&height=1833",tags:["回忆","博客"]},n=`<h1>前言</h1>
+<p>前不久我发现了一个神奇的网站，大概叫网站历史快照，我发现他居然有记录我的网站，那就分享一下网站是<a href="https://web.archive.org/">点击前往</a></p>
+<p>:::Accordion{23 年的样式}
+:::Diff
+<img src="https://wp-cdn.4ce.cn/v2/YpEFzyn.png" alt="alt text">
+vs
+<img src="https://wp-cdn.4ce.cn/v2/XCTjQx4.png" alt="alt text">
+:::
+:::
+:::Accordion{24 年的样式}
+<img src="https://wp-cdn.4ce.cn/v2/AmUk3xn.png" alt="alt text">
+:::
+总体来说额，好像全是丑照，期间其他时间基本就是使用框架之类的</p>
+`,c=[{level:"1",content:"&#x524D;&#x8A00;"}];export{t as attributes,n as html,c as toc};
