dc08d3192724d6742ed793da4120bb41af3d70ab
MLeaf-coder <MLeaf-coder@users.noreply.github.com>
2025-07-15 07:18:59 +0000

    deploy: cae3a680fc9d1f27137d0a54323f0b5d5d5335c3

--- assets/blog-DQKB27km.js
+++ assets/blog-DQKB27km.js
@@ -1,5 +1,6 @@
-const e={title:"博客",published:"2024-12-13T00:00:00.000Z",image:"/assets/img/bg.jpg"},t=`<h1>博客</h1>
+const e={title:"博客",published:"2024-12-13T00:00:00.000Z",image:"/assets/img/bg.jpg"},p=`<h1>博客</h1>
 <p>博客本身就是记录开发经验，记录生活的地方，但我想要的是一个简洁而不简单的</p>
+<p>抱歉最近各位应该都被定向到我的这个博客了，原因很简单我部署位置变化导致的，后续会将文章迁移，之前是一个域名共存两个博客，现在是被全部拦截了</p>
 <h2>后续</h2>
 <p>想到什么开发什么，缺少什么编写什么。</p>
 <p>样式已经完成大部分了，可以在博文的样式测试的文章中看到所以样式效果
@@ -8,4 +9,4 @@ const e={title:"博客",published:"2024-12-13T00:00:00.000Z",image:"/assets/img/
 <p><a href="https://fy.unaux.com/">博客副</a> <a href="https://fengyegf.cn/blog">博客</a></p>
 <p><a href="https://t.me/FY_YUN">高德</a></p>
 <p>过几天在写一下侧边栏吧</p>
-`,n=[{level:"1",content:"&#x535A;&#x5BA2;"},{level:"2",content:"&#x540E;&#x7EED;"}];export{e as attributes,t as html,n as toc};
+`,t=[{level:"1",content:"&#x535A;&#x5BA2;"},{level:"2",content:"&#x540E;&#x7EED;"}];export{e as attributes,p as html,t as toc};
