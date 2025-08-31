fed8fda77576bd5468f168185292aad34ff770d0
fengyegf <106392719+fengyegf@users.noreply.github.com>
2025-09-01 00:49:37 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fengyegf.cn',
-  base: "/blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
